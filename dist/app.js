1456a935d08f5f88700487af59ed0d7fb800de30
Ochiengsteven <stevenochieng6305@gmail.com>
2023-06-29 11:43:29 +0300

    add the humburger menu for mobile view

--- dist/app.js
+++ dist/app.js
@@ -8,3 +8,25 @@ const minuteClick = () => {
 const hourClick = () => {
     btn.style.left = '0';
 }
+
+const mainMenu = document.querySelector('#mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const portfolio = document.querySelector('a[href="#my-work"]');
+const aboutMe = document.querySelector('a[href="#about-me"]');
+const contactMe = document.querySelector('a[href="#contact-me"]');
+
+function show() {
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
+}
+
+function close() {
+  mainMenu.style.top = '-100%';
+}
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+portfolio.addEventListener('click', close);
+aboutMe.addEventListener('click', close);
+contactMe.addEventListener('click', close);
